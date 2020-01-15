7f4a6fa582af063321190d0f8462fdab8e00e8b7
Bruno Fantinelli <bruno.fantinelli0@gmail.com>
2020-01-15 20:11:20 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CDS_API_FIREBASE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1 - Cadastrar Novo Usuario.");
+                Console.WriteLine("2 - Buscar Usuario.");
+                Console.WriteLine("3 - Sair.");
+                Console.Write("OP: ");
+                String op = Console.ReadLine();
+                switch (op)
+                {
+                    case "1":
+                        User aux = new User();
+                        Console.Write("ID: ");
+                        aux.id = Console.ReadLine();
+                        Console.Write("Nome: ");
+                        aux.nome = Console.ReadLine();
+                        Console.Write("Telefone: ");
+                        aux.telefone = Console.ReadLine();
+                        Console.Write("Endereco: ");
+                        aux.endereco = Console.ReadLine();
+                        Console.Write("Email: ");
+                        aux.email = Console.ReadLine();
+                        cadastrarUser(aux);
+                        break;
+
+                    case "2":
+                        Console.Write("ID: ");
+                        User busca = buscarUser(Console.ReadLine());
+                        if(busca != null)
+                        {
+                            Console.WriteLine("ID: " + busca.id);
+                            Console.WriteLine("Nome: " + busca.nome);
+                            Console.WriteLine("Telefone: " + busca.telefone);
+                            Console.WriteLine("Endereco: " + busca.endereco);
+                            Console.WriteLine("Email: " + busca.email);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Usuario Inexistente");
+                        }
+                        break;
+
+                    case "3":
+                        return;
+                    
+                    default:
+                        Console.WriteLine("Opcao Invalida.");
+                        break;
+
+                }
+                
+            }
+        }
+
+        public static void cadastrarUser(User user)
+        {
+            string userJson = JsonSerializer.Serialize(user);
+            var request = WebRequest.CreateHttp("https://cds-firebase.firebaseio.com/" + user.id + ".json");
+            request.Method = "PUT";
+            request.ContentType = "application/json";
+            var buffer = Encoding.UTF8.GetBytes(userJson);
+            request.ContentLength = buffer.Length;
+            request.GetRequestStream().Write(buffer, 0, buffer.Length);
+            request.GetResponse();
+
+        }
+
+        public static User buscarUser(String id)
+        {
+            WebClient wc = new WebClient();
+            String resultado = wc.DownloadString("https://cds-firebase.firebaseio.com/" + id + ".json");
+            User aux = JsonSerializer.Deserialize<User>(resultado);
+            return aux;
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CDS_API_FIREBASE
+{
+    class User
+    {
+        public string id { get; set; }
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string endereco { get; set; }
+        public string email { get; set; }
+    }
+}
